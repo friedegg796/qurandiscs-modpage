8f8fea2540104b5e79250a55dd1528f125e7b461
Hlodr2 <ohi10@outlook.com>
2026-04-25 21:44:50 +0000

    Apr 25, 2026, 5:44 PM

--- src/pages/MuslimcraftChanges.jsx
+++ src/pages/MuslimcraftChanges.jsx
@@ -9,10 +9,13 @@ function MuslimcraftChanges(){
 
      <h2> Faces </h2>
        <p> Apparently I need to reread the TOS though because I dont know if this is allowed</p>
+       <p> This is not changed in the lite version btw </p>
 
 
             <h2> Music </h2>
-       <p> Music has been GONED! (atleast it should've been) including jukebox discs and the background music</p>
+       <p> Music has been GONED! (atleast it should've been) including jukebox discs and the background music </p>
+
+       
 
     </div>
     </>
