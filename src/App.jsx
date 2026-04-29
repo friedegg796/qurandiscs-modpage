7810415b79d4db4079631bc361d894306b155269
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-04-29 15:29:22 +0000

    Wed, Apr 29, 2026, 11:29 AM -04:00

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { HashRouter, Routes, Route } from "react-router-dom"
 import Index from './pages/Index.jsx'
 import Changes from './pages/Changes.jsx'
 import MuslimcraftDownloads from './pages/MuslimcraftDownloads.jsx'
-import ArsMuslimDownloads from './pages/ArsMuslimDownloads.jsx'
-import Footer from '/workspaces/214691060/muslimdiscs-modpage/src/componenets/Footer.jsx'
+//import ArsMuslimDownloads from './pages/ArsMuslimDownloads.jsx'
+//import Footer from '/workspaces/214691060/muslimdiscs-modpage/src/componenets/Footer.jsx'
 
 function App() {
 
