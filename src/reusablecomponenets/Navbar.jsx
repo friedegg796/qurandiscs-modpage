bb2fe5c3907ffbaafd9d2e1f195e4195e3d234a2
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-05-02 18:02:51 +0000

    Sat, May 2, 2026, 2:02 PM -04:00

--- src/reusablecomponenets/Navbar.jsx
+++ src/reusablecomponenets/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return(
         <>
         <nav>
-            <div class="title"> Muslimdiscs </div>
+            <Link to="/"><div class="title"> Muslimdiscs </div></Link>
             <ul>
                {/* <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li> */}
